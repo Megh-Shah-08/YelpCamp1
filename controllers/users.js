78c77fc82e272981ff51281df176a28b3f98864f
Megh-Shah-08 <meghshah810@gmail.com>
2024-12-20 23:19:21 +0530

    Added a console to check if user is authenticated or not

--- controllers/users.js
+++ controllers/users.js
@@ -29,6 +29,7 @@ module.exports.renderLoginForm = (req, res) => {
 
 //login the user
 module.exports.loginUser = async (req, res) => {
+    console.log(req.isAuthenticated());
     req.flash('success', "Welcome Back!");
     const redirectUrl = res.locals.returnTo || '/campgrounds';
     delete req.session.returnTo;
